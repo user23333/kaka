4e6628a47b6e98e0ab70a9e04d78e68104a63300
user23333 <43466088+user23333@users.noreply.github.com>
2024-11-24 02:30:23 +0800

    Update panel.py
    Useless

--- panel.py
+++ panel.py
@@ -4,7 +4,6 @@ import requests
 from datetime import datetime, timedelta
 from pyquery import PyQuery as pq
 import user_agent
-# TODO: Try using cfscrape or curl_cffi
 
 v1 = os.getenv('V1')
 v2 = os.getenv('V2')
